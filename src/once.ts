f98d275536e6c748a0a429ed683bdf6325e66431
Aaron <aaron@Aarons-MacBook-Pro.local>
2025-06-23 12:20:49 +0800

    init

---
+++ src/once.ts
@@ -0,0 +1,34 @@
+import fs from "fs";
+import path from "path";
+
+import {
+  OUTPUT_DIR
+} from "./constants";
+
+const panic = <T>(message: string): T => {
+  throw new Error(message);
+};
+
+// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+// @ts-ignore-next-line
+type Tree = Record<string, string | Tree>
+
+const writeTree = (parentDir: string, tree: Tree) => {
+  if (!fs.existsSync(parentDir)) fs.mkdirSync(parentDir);
+
+  for (const [k, v] of Object.entries(tree)) {
+    const prefix = path.join(parentDir, k);
+
+    if (typeof v === "string") {
+      fs.writeFileSync(`${prefix}.txt`, v);
+    } else {
+      writeTree(prefix, v);
+    }
+  }
+};
+
+const data = {
+  "test": "123",
+}
+
+fs.writeFileSync(path.join(OUTPUT_DIR, "test.json"), JSON.stringify(data, null, 2));
