3b32c2ee4e41a10bf1e8ee7bc26abdc0a146087b
momo <momo@apeark.com>
2025-06-24 17:59:31 +0800

    record usdc wallet data

--- src/fetchPortfolio.ts
+++ src/fetchPortfolio.ts
@@ -7,7 +7,7 @@ export const fetchPortfolio = async (user: string) => {
   const clearinghouseState = await sdk.info.perpetuals.getClearinghouseState(user);
   const spotBalances = await sdk.info.spot.getSpotClearinghouseState(user);
   const delegations = await sdk.info.getDelegations(user);
-  
+
   const delegationsBalance = delegations.reduce((acc, delegation) => acc + Number(delegation.amount), 0);
   const spotMid = Number(allMids["HYPE-SPOT"]);
   const perpMid = Number(allMids["HYPE-PERP"]);
@@ -15,9 +15,11 @@ export const fetchPortfolio = async (user: string) => {
   const hypeSpotBalance = Number(spotBalances.balances.find((b) => b.coin === "HYPE-SPOT")?.total || 0);
   const hypePerpBalance = Number(hypePerpPosition?.szi || 0);
   const usdcSpotBalance = Number(spotBalances.balances.find((b) => b.coin === "USDC-SPOT")?.total || 0);
+  const usdcPerpWithdrawableBalance = Number(clearinghouseState.withdrawable || 0);
+  const usdcWithdrawableBalance = usdcSpotBalance + usdcPerpWithdrawableBalance;
   const perpAccountValue = Number(clearinghouseState.marginSummary.accountValue || 0);
   const cumFunding = Number(hypePerpPosition?.cumFunding.allTime || 0);
-   
+
   const data = {
     spotMid,
     perpMid,
@@ -26,6 +28,7 @@ export const fetchPortfolio = async (user: string) => {
     usdcSpotBalance,
     perpAccountValue,
     cumFunding,
+    usdcWithdrawableBalance,
     accountValue: ((hypeSpotBalance + delegationsBalance) * spotMid) + usdcSpotBalance + perpAccountValue,
     stakingDelegations: delegations,
   }
