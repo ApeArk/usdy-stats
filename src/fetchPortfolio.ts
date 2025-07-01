83cc269755242ab5aad46a0997d5bc445d4a406a
Aaron <aaron@Aarons-MacBook-Pro.local>
2025-07-01 15:28:59 +0800

    add apr7d and apr30d

--- src/fetchPortfolio.ts
+++ src/fetchPortfolio.ts
@@ -23,11 +23,22 @@ const getFundingHistory = async (user: string, startTime: Date) => {
   return funding;
 }
 
+const getPortfolio = async (user: string) => {
+  const portfolio = await sdk.info.portfolio(user);
+  const avg7d = portfolio[1][1].accountValueHistory.reduce((acc, entry) => acc + Number(entry[1]), 0) / portfolio[1][1].accountValueHistory.length;
+  const avg30d = portfolio[2][1].accountValueHistory.reduce((acc, entry) => acc + Number(entry[1]), 0) / portfolio[2][1].accountValueHistory.length;
+  return {
+    avg7d,
+    avg30d,
+  }
+}
+
 export const fetchPortfolio = async (user: string) => {
   const allMids = await sdk.info.getAllMids();
   const clearinghouseState = await sdk.info.perpetuals.getClearinghouseState(user);
   const spotBalances = await sdk.info.spot.getSpotClearinghouseState(user);
   const delegations = await sdk.info.getDelegations(user);
+  const portfolio = await getPortfolio(user);
 
   const startTime7d = new Date(Date.now() - 7 * 24 * 60 * 60 * 1000);
   const cumFunding7d = await getFundingHistory(user, startTime7d);
@@ -43,7 +54,12 @@ export const fetchPortfolio = async (user: string) => {
   const hypePerpBalance = Number(hypePerpPosition?.szi || 0);
   const usdcSpotBalance = Number(spotBalances.balances.find((b) => b.coin === "USDC-SPOT")?.total || 0);
   const perpAccountValue = Number(clearinghouseState.marginSummary.accountValue || 0);
-  const cumFunding = Number(hypePerpPosition?.cumFunding.allTime || 0);
+  const cumFunding = -Number(hypePerpPosition?.cumFunding.allTime || 0);
+  const avg7d = portfolio.avg7d;
+  const avg30d = portfolio.avg30d;
+  const apr7d = cumFunding7d / avg7d / 7 * 365 * 100;
+  const apr30d = cumFunding30d / avg30d / 30 * 365 * 100;
+  const accountValue = ((hypeSpotBalance + delegationsBalance) * spotMid) + usdcSpotBalance + perpAccountValue;
 
   const data = {
     spotMid,
@@ -55,7 +71,11 @@ export const fetchPortfolio = async (user: string) => {
     cumFunding,
     cumFunding7d,
     cumFunding30d,
-    accountValue: ((hypeSpotBalance + delegationsBalance) * spotMid) + usdcSpotBalance + perpAccountValue,
+    avg7d,
+    avg30d,
+    apr7d,
+    apr30d,
+    accountValue,
     stakingDelegations: delegations,
   }
   return data;
